64df3c5a8d8e89a0cd8fd06844702c71c5f8f8ce
Ryan Yovanda <ryanyovanda@gmail.com>
2025-03-10 16:37:13 +0700

    build eslint fix

--- src/app/(auth)/register-tenant/page.tsx
+++ src/app/(auth)/register-tenant/page.tsx
@@ -20,7 +20,7 @@ const Register = () => {
     email: Yup.string().email('Invalid email').required('Email is required'),
     password: Yup.string().min(6, 'Password must be at least 6 characters').required('Password is required'),
     confirmPassword: Yup.string()
-      .oneOf([Yup.ref('password'), null], 'Passwords must match')
+      .oneOf([Yup.ref('password'), undefined], 'Passwords must match') // ✅ FIXED
       .required('Confirm Password is required')
   });
 
