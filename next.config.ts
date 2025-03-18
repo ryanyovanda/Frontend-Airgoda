f68b357a85c69addfa913e1a12d64dee59434166
Ryan Yovanda <ryanyovanda@gmail.com>
2025-03-18 14:27:13 +0700

    fix path

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,11 @@
 import type { NextConfig } from "next";
 import path from "node:path"; 
 
-/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   reactStrictMode: true, 
-  swcMinify: true,
 
   images: {
     domains: ["res.cloudinary.com"], 
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-    ],
   },
 
   eslint: {
@@ -24,11 +16,10 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL, 
   },
 
-
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      "@": path.resolve(__dirname, "src"), // ✅ Fix for Vercel builds
+      "@": path.resolve(process.cwd(), "src"), // ✅ Fix for Vercel builds
     };
     return config;
   },
